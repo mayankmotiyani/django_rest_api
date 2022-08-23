bebf135141953453d5b9b2e039d0f6abde40b3d1
mayank <mayank.infograins@gmail.com>
2022-08-23 12:11:39 +0530

    project for learning

---
+++ django_rest_app/serializers/serializers.py
@@ -0,0 +1,8 @@
+from django_rest_app.models import (
+    Snippet
+)
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
